caf5f62fc8f4fc56c6fa25d737fda36f59c2adc4
Euler Alvarenga <euleralvarenga@eulers-mac-mini.intranet.eem>
2019-09-20 15:22:40 -0300

    fix portrait and landscape tabbar error

--- whitelabel/src/components/TabBar.js
+++ whitelabel/src/components/TabBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   View,
   Text,
@@ -9,6 +9,7 @@ import {
 import {colors} from '~/styles';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
 
 const S = StyleSheet.create({
   container: {flexDirection: 'row', height: 52, elevation: 2},
@@ -31,64 +32,102 @@ const S = StyleSheet.create({
     padding: 10,
     position: 'absolute',
     top: -27,
-    left: screenWidth / 2 - 25,
+    alignSelf: 'center',
     width: 50,
     height: 50,
     backgroundColor: colors.primary,
   },
 });
 
-const TabBar = props => {
-  const {
-    renderIcon,
-    getLabelText,
-    activeTintColor,
-    inactiveTintColor,
-    onTabPress,
-    onTabLongPress,
-    getAccessibilityLabel,
-    navigation,
-  } = props;
+class TabBar extends React.Component {
+  constructor() {
+    super();
 
-  const {routes, index: activeRouteIndex} = navigation.state;
-  console.log('Props', props);
-  return (
-    <View style={S.container}>
-      {routes.map((route, routeIndex) => {
-        const isRouteActive = routeIndex === activeRouteIndex;
-        const tintColor = isRouteActive ? activeTintColor : inactiveTintColor;
-        return routeIndex === 1 ? (
-          <TouchableOpacity
-            key={routeIndex}
-            style={S.tabButton2}
-            onPress={() => {
-              //onTabPress({route}); // controla rotas
-            }}
-            onLongPress={() => {
-              //onTabLongPress({route}); // controla rotas
-            }}
-            accessibilityLabel={getAccessibilityLabel({route})}>
-            {renderIcon({route, focused: isRouteActive, tintColor})}
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            key={routeIndex}
-            style={S.tabButton}
-            onPress={() => {
-              onTabPress({route});
-            }}
-            onLongPress={() => {
-              onTabLongPress({route});
-            }}
-            accessibilityLabel={getAccessibilityLabel({route})}>
-            {renderIcon({route, focused: isRouteActive, tintColor})}
+    /**
+     * Returns true if the screen is in portrait mode
+     */
+    isPortrait = () => {
+      const dim = Dimensions.get('screen');
+      return dim.height >= dim.width;
+    };
 
-            {/* <Text>{getLabelText({route})}</Text> */}
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
-};
+    this.state = {
+      orientation: isPortrait() ? 'portrait' : 'landscape',
+    };
+
+    // Event Listener for orientation changes
+    Dimensions.addEventListener('change', () => {
+      this.setState(
+        {
+          orientation: isPortrait() ? 'portrait' : 'landscape',
+        },
+        () => {
+          console.log('state', this.state);
+        },
+      );
+    });
+  }
+
+  render() {
+    const {
+      renderIcon,
+      getLabelText,
+      activeTintColor,
+      inactiveTintColor,
+      onTabPress,
+      onTabLongPress,
+      getAccessibilityLabel,
+      navigation,
+    } = this.props;
+
+    const {routes, index: activeRouteIndex} = navigation.state;
+
+    return (
+      <View style={S.container}>
+        {routes.map((route, routeIndex) => {
+          const isRouteActive = routeIndex === activeRouteIndex;
+          const tintColor = isRouteActive ? activeTintColor : inactiveTintColor;
+          return routeIndex === 1 ? (
+            <TouchableOpacity
+              key={routeIndex}
+              style={[
+                S.tabButton2,
+                {
+                  left:
+                    this.state.orientation === 'landscape'
+                      ? screenHeight / 2 - 25
+                      : screenWidth / 2 - 25,
+                },
+              ]}
+              onPress={() => {
+                //onTabPress({route}); // controla rotas
+              }}
+              onLongPress={() => {
+                //onTabLongPress({route}); // controla rotas
+              }}
+              accessibilityLabel={getAccessibilityLabel({route})}>
+              {renderIcon({route, focused: isRouteActive, tintColor})}
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              key={routeIndex}
+              style={S.tabButton}
+              onPress={() => {
+                onTabPress({route});
+              }}
+              onLongPress={() => {
+                onTabLongPress({route});
+              }}
+              accessibilityLabel={getAccessibilityLabel({route})}>
+              {renderIcon({route, focused: isRouteActive, tintColor})}
+
+              {/* <Text>{getLabelText({route})}</Text> */}
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  }
+}
 
 export default TabBar;
